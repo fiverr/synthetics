9a9db21844ceba2c1c90e0b88e5d8a879e659da5
Omri <omrilotan@users.noreply.github.com>
2018-02-12 11:43:51 +0200

    Start punching

---
+++ README.md
@@ -0,0 +1,10 @@
+# Punch and Judy
+
+## Fiverr page measurer
+
+### Install
+```sh
+nvm use
+npm i
+npm start
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+require('colors');
+const inquirer = require('inquirer');
+const table = require('./lib/table');
+const page = require('./lib/page');
+
+const ROOT = 'https://www.fiverr.com';
+
+const pages = require('./pages.json').map(page => Object.assign(page, { url: ROOT + page.url}));
+
+try {
+    (async () => {
+        const { tests } = await inquirer
+            .prompt([
+                {
+                    name: 'tests',
+                    message: 'Measure Pages',
+                    type: 'checkbox',
+                    choices: pages.map(page => ({
+                        name: page.name,
+                        value: page,
+                    })),
+                },
+            ]);
+
+        const results = table(
+            await Promise.all(
+                tests.map(
+                    async test => await page(test)
+                )
+            )
+        );
+
+        console.log(`\n${results}`);
+    })();
+} catch (error) {
+    console.error(error);
+    return;
+}
+
+
+
